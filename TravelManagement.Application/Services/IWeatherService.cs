d6f311e585e76fc824fa293b7a83f7a8b7598a40
Sajjad <s.ebrahimiraad@gmail.com>
2023-04-27 14:51:33 +0430

    external weather service

---
+++ TravelManagement.Application/Services/IWeatherService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Application.DTO.External;
+using TravelManagement.Domain.ValueObjects;
+
+namespace TravelManagement.Application.Services
+{
+    public interface IWeatherService
+    {
+        Task<WeatherDto> GetWeatherAsync(Destination localization);
+    }
+}
