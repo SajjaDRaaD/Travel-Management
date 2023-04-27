67bc2ec80544b3a993bf933c633ffb3b8eea84cd
Sajjad <s.ebrahimiraad@gmail.com>
2023-04-27 15:05:11 +0430

    commands

---
+++ TravelManagement.Application/Commands/CreateTravelerCheckListWithItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Domain.Consts;
+using TravelManagement.Shared.Abstractions.Commands;
+
+namespace TravelManagement.Application.Commands
+{
+    public record CreateTravelerCheckListWithItem(Guid id, string name, ushort days, Gender gender, DestinationWriteModel destination):ICommand;
+
+    public record DestinationWriteModel(string city, string country);
+}
---
+++ TravelManagement.Application/Commands/RemoveTravelerItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Shared.Abstractions.Commands;
+
+namespace TravelManagement.Application.Commands
+{
+    public record RemoveTravelerItem(Guid travelerCheckListId, string name):ICommand;
+}
