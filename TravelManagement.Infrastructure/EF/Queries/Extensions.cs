507c9efdb6aea62a874e4abf642c564683cb5ba4
Sajjad <s.ebrahimiraad@gmail.com>
2023-05-01 19:52:25 +0430

    extension method for query

---
+++ TravelManagement.Infrastructure/EF/Queries/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Application.DTO;
+using TravelManagement.Infrastructure.EF.Models;
+
+namespace TravelManagement.Infrastructure.EF.Queries
+{
+    internal static class Extensions
+    {
+        public static TravelerCheckListDto AsDto(this TravelerCheckListReadModel readModel)
+        {
+            return new TravelerCheckListDto()
+            {
+                Id = readModel.Id,
+                Name = readModel.Name,
+                Destination = new DestinationDto()
+                {
+                    City = readModel.Destination.City,
+                    Country = readModel.Destination.Country,
+                },
+                Items = readModel.Items?.Select(x => new TravelerItemDto
+                {
+                    Name = x.Name,
+                    Quantity = x.Quantity,
+                    IsTaken = x.IsTaken
+                })
+            };
+        }
+    }
+}
