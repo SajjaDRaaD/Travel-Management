0593d74854d4b0807324f00907522f1c9dbf64df
Sajjad <s.ebrahimiraad@gmail.com>
2023-05-03 03:05:55 +0430

    dumb weather service

---
+++ TravelManagement.Infrastructure/Services/DumbWeatherService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Application.DTO.External;
+using TravelManagement.Application.Services;
+using TravelManagement.Domain.ValueObjects;
+
+namespace TravelManagement.Infrastructure.Services
+{
+    public sealed class DumbWeatherService : IWeatherService
+    {
+        public Task<WeatherDto> GetWeatherAsync(Destination localization)
+            => Task.FromResult(new WeatherDto(new Random().Next(5,30)));
+    }
+}
