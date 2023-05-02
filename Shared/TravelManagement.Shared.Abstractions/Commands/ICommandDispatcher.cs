e91e7541828ea0cc48d2b9afb28ac32f3c848396
Sajjad <s.ebrahimiraad@gmail.com>
2023-05-03 03:40:47 +0430

    command and query dispatcher

---
+++ Shared/TravelManagement.Shared.Abstractions/Commands/ICommandDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelManagement.Shared.Abstractions.Commands
+{
+    public interface ICommandDispatcher
+    {
+        Task DispacheAsync<TCommand>(TCommand command) where TCommand : class, ICommand;
+    }
+}
---
+++ TravelManagment.Shared/Commands/InMemoryCommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelManagement.Shared.Abstractions.Commands;
+
+namespace TravelManagement.Shared.Commands
+{
+    internal sealed class InMemoryCommandDispatcher : ICommandDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InMemoryCommandDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        async Task ICommandDispatcher.DispacheAsync<TCommand>(TCommand command)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+            await handler.HandleAsync(command);
+        }
+    }
+}
